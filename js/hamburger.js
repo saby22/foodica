2a78edb5d7657baa5639abb8532664bb24cb19d0
Saby22 <sabyasachi.tffs@gmail.com>
2020-09-25 11:06:17 +0530

    Added the favicon to all the pages

--- js/hamburger.js
+++ js/hamburger.js
@@ -2,10 +2,6 @@ const burger = document.querySelector(".burger");
 
 
 burger.addEventListener("click", function(e){
-    // console.dir(burger);
-    // burger.children[1].classList.toggle("line2-hide");
-    // burger.children[0].classList.toggle("top-line");
-    // burger.children[2].classList.toggle("bottom-line");
     burger.classList.toggle("open");
 
 });
