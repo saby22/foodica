9bc3be4d9c9539ce0c27f27202dca747a14b23ca
Saby22 <sabyasachi.tffs@gmail.com>
2020-09-25 13:16:05 +0530

    added the hamburger menu with animation

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,7 +1,31 @@
 const burger = document.querySelector(".burger");
-
+const burgerMenu = document.querySelector(".burger-links");
+const body = document.querySelector("body");
 
 burger.addEventListener("click", function(e){
     burger.classList.toggle("open");
 
+    if(burger.classList.contains("open") && burger.style.display !== "none"){
+        burgerMenu.classList.add("burger-on");
+        body.style.overflow = "hidden";
+    }
+    else{
+        burgerMenu.classList.remove("burger-on");
+        body.style.overflow = "visible";
+    }
+});
+
+window.addEventListener("resize",function(){
+
+    if(window.innerWidth > 810){
+        if(burger.classList.contains("open")){
+            burger.classList.remove("open");
+        }
+        if(burgerMenu.classList.contains("burger-on")){
+            burgerMenu.classList.remove("burger-on");
+        }
+        if(body.style.overflow === "hidden"){
+            body.style.overflow = "visible";
+        }
+    }
 });
