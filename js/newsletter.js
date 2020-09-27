b2d0d0d5365f70819eb0712f5b5511db30f0534d
ishika1403 <ishikamukherjee14@gmail.com>
2020-09-27 22:08:19 +0530

    Added validation to newsletter

--- js/newsletter.js
+++ js/newsletter.js
@@ -1,23 +1,34 @@
-const email-news = documentgetElementById("email-news");
+const emailNews = document.getElementById("email-news");
 const form = document.querySelector("form");
+const container = document.querySelector(".newsletter-container");
+const content = document.querySelector(".newsletter-content");
 const regem = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 ;
 
 form.addEventListener('submit',function(e){
     
 
-    if(email-news.value.trim() === ""){
-        // console.dir(form);
-        form.children[1].children[2].classList.remove("hidden");
-        email.focus()
+    if(emailNews.value.trim() === ""){
+        console.dir(content);
+        // container.children[1].classList.remove("hidden");
+        content.children[3].classList.remove("hidden");
+        emailNews.focus()
         e.preventDefault();
     }
 
-    else if(regem.test(email-news.value) == false){
-        form.children[1].children[3].classList.remove("hidden");
-        email.focus();
+    else if(regem.test(emailNews.value) == false){
+        content.children[4].classList.remove("hidden");
+        emailNews.focus();
         e.preventDefault();
     }
 
 
+});
+
+emailNews.addEventListener('input',function(){
+    content.children[3].classList.add("hidden");
+});
+
+emailNews.addEventListener('input',function(){
+    content.children[4].classList.add("hidden");
 });
